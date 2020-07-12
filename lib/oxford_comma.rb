d28184af35cfbfc657a9f77bd63813333f5b1e7d
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 14:52:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+#require 'pry'
 def oxford_comma(array)
-
+array == []
+case array.size
+when 1
+array = array.join
+when 2
+array.join(' and ')
+when array.length > 3
+  array[-1] = "and #{array[-1]}"
+array.join(' , ')
+#binding.pry
 end
+end
+      
