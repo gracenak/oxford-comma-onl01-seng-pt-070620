f2f1f56c052ae64771c671e884606f291034d440
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 14:59:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
 #require 'pry'
 def oxford_comma(array)
 array == []
-case array.size
-when 1
-array = array.join
-when 2
-array.join(' and ')
-when array.length > 3
+if array.length == 1
+  array = array.join
+elsif array.length == 2
+  array.join(' and ')
+elsif array.length >= 3
   array[-1] = "and #{array[-1]}"
-array.join(' , ')
+  array.join(' , ')
 #binding.pry
 end
 end
